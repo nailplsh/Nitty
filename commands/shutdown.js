7d85445f418f07cbd831ddf7ea8b9afc9adac7a7
hazelnitwit <hazelnitwit@shaw.ca>
2023-01-20 09:25:29 -0500

    changes made to commands/shutdown.js and package.json

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -4,15 +4,18 @@ const { exit } = require('node:process');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shutdown')
-		.setDescription('Admin Only.'),
+		.setDescription('Only the server owner can do this.'),
 		async execute(interaction) {
-			if (interaction.user.id != '354414172559114240') {
-				interaction.reply('Sorry, you don\'t have access to this feature.').then(() => {
+			if (interaction.guild.ownerId !== interaction.member.id) {
+				interaction.reply('Sorry, you must be the owner of this server.').then(() => {
 					console.log(`${interaction.member.displayName} tried to shut down the bot. ID: ${interaction.user.id}`);
 					return;
 				});
 			} else {
-				interaction.reply('Nitty is shutting down!').then(() => {
+				interaction.reply({
+					content: 'Nitty is shutting down.',
+					ephemeral: true,
+				}).then(() => {
 					console.log('Nitty is shutting down.');
 					exit(0);
 				});

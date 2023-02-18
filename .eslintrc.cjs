1015d3fb1249e8c9fad5b427e7a845b7b1909a5f
yi5 <github@yi5.dev>
2023-02-18 16:26:50 +0100

    Adding lint file and change in gitignore
    Added the lint cjs and changed the gitignore to ignore eslintrc.json file

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+    },
+    root: true,
+};

984d737d5afb92cbf05faa1da2d65cd245b13c75
yi5 <github@yi5.dev>
2023-02-19 15:43:51 +0100

    Added yi5 Command

---
+++ src/commands/yi5.ts
@@ -0,0 +1,34 @@
+import { SlashCommandBuilder, EmbedBuilder, CommandInteraction } from "discord.js";
+// import requests
+import axios from 'axios';
+
+async function getGithubInfos(username: string) {
+    return await axios.get(`https://api.github.com/users/${username}`)
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('yi5')
+        .setDescription('Sends information about the Yi5'),
+
+
+    async execute(interaction: CommandInteraction) {
+        const githubInfos = await getGithubInfos('LagKnowsWhy').then(res => res.data)
+
+        const embed = new EmbedBuilder()
+            .setTitle('Yi5')
+            .setURL("https://github.com/LagKnowsWhy")
+            .setColor('#0099ff')
+            .setDescription(`Yi5 is a bot developer, and a contributor of this Bot.
+            \n**Github Stats**:
+            Public repos: ${githubInfos.public_repos}
+            Followers: ${githubInfos.followers}
+            \n**Github**: https://github.com/LagKnowsWhy
+            **Discord**: yi5#1057`)
+            .setThumbnail(`https://avatars.githubusercontent.com/u/${githubInfos.id}?v=4`)
+
+        return await interaction.reply({
+            embeds: [embed],
+        })
+    }
+}
